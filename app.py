cdbcc3164c4304d4f62a1dde26eb93ee0b8789df
akhilbisht208-creator <akhilbisht208@gmail.com>
2026-08-19 00:29:53 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,5 @@
+#Add 25 to the list.Sort the list.Print the final list
+numbers = [10, 5, 20, 8, 15]
+numbers.append(25)
+numbers.sort()
+print(numbers)
---
+++ tupleindexing.py
@@ -0,0 +1,5 @@
+#tuple indexing and print  "Akhil", "Python", 95
+data = ("Akhil", 18, "Python", 95, "India")
+print(data[0])
+print(data[2])
+print(data[3])
